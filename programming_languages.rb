1587eefdd765b0448140779ad80e5b460308dd4a
smithwebtek <brad@smithwebtek.com>
2018-04-15 09:08:22 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,84 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+	new_hash = {}
+	languages.each do |style, langs|
+		langs.each do |lang, type|
+			if new_hash[lang]
+				new_hash[lang][:style].push(style)
+			else
+				new_hash[lang] = {
+					:type => type[:type],
+					:style => []
+				}
+				new_hash[lang][:style].push(style)
+			end
+		end
+	end
+	new_hash
 end
+
+
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
+
+
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+ 
+#   }
+# }
